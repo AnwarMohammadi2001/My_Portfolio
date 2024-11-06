719af7f37ee287a18f15beaa62701b75d627d9f5
Anwar <anwarmohammadi1390@gmail.com>
2024-11-06 18:27:06 +0430

    add

--- Backend/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/contactRoutes.js
-const express = require("express");
-const router = express.Router();
-const { createContact } = require("../controllers/contactController");
-
-router.post("/contact", createContact);
-
-module.exports = router;
